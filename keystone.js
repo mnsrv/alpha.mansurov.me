2571a8b4e9e57a07ef13ccaa555b305c882215b9
Aleksandr Mansurov <am@idaproject.com>
2016-07-10 18:49:42 +0300

    add dotenv

--- keystone.js
+++ keystone.js
@@ -1,3 +1,7 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').config();
+
 var keystone = require('keystone');
 var Twig = require('twig');
 Twig.cache(false);
