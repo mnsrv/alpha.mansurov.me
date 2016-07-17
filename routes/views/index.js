bae42ef9482e401ec53746510363523f6ad9386c
Aleksandr Mansurov <am@idaproject.com>
2016-07-18 01:23:51 +0300

    projects to index
    remove changelog

--- routes/views/index.js
+++ routes/views/index.js
@@ -3,5 +3,24 @@ var keystone = require('keystone');
 module.exports = function(req, res) {
   var view = new keystone.View(req, res);
 
+  var locals = res.locals;
+  locals.section = 'index';
+  locals.data = {
+		projects: [],
+	};
+
+  // Load projects
+	view.on('init', function (next) {
+    var Project = keystone.list('Project')
+
+    var q = Project.model.find();
+		q.where('state', 'продакшен').sort('-publishedDate')
+
+		q.exec(function (err, results) {
+			locals.data.projects = results;
+			next(err);
+		});
+	});
+
   view.render('index');
 }
