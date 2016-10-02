f284921a07571829adb58508161964b1bd0112db
Aleksandr Mansurov <am@idaproject.com>
2016-10-02 22:47:02 +0300

    add buggyfill for ios vh

--- client/main.js
+++ client/main.js
@@ -1,4 +1,8 @@
 require('./css/main.scss');
+var viewportUnitsBuggyfill = require('viewport-units-buggyfill');
+viewportUnitsBuggyfill.init();
+// viewportUnitsBuggyfill.findProperties();
+// var cssText = viewportUnitsBuggyfill.getCss();
 var $ = require('jquery');
 
 console.log('Hello world!');
