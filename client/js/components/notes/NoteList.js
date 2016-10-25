43314ec3a40d41a6072c92d32c6f0aca46804dfb
Aleksandr <am@idaproject.com>
2016-10-26 01:00:22 +0300

    add drag window
    close #11

--- client/js/components/notes/NoteList.js
+++ client/js/components/notes/NoteList.js
@@ -1,37 +1,42 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router';
+import React from 'react';
+import Draggable from 'react-draggable';
 import NoteMenuItem from './NoteMenuItem';
 import NoteItem from './NoteItem';
 import WindowMenu from '../WindowMenu';
 
 const NoteList = (props) =>
-  <div className="window">
-    <div className={`window__header${props.menuOpened ? ' is-menu-opened' : ''}`}>
-      <div className="window__circles">
-        <div className="window__circle window__circle_red"></div>
-        <div className="window__circle window__circle_yellow"></div>
-        <div className="window__circle window__circle_green"></div>
-      </div>
-      <WindowMenu />
-      <p className="window__title" onClick={props.showMenu}>Заметки</p>
-    </div>
-    <div className="window__body">
-      <div className="notes">
-        <div className="notes__list">
-          {props.notes.map(note =>
-            <NoteMenuItem
-              key={note._id}
-              note={note}
-              onClick={props.onNoteClick}
-              activeSlug={props.activeNote ? props.activeNote.slug : null }
-            />
-          )}
+  <Draggable
+    handle=".window__header"
+    bounds="body"
+  >
+    <div className="window">
+      <div className={`window__header${props.menuOpened ? ' is-menu-opened' : ''}`}>
+        <div className="window__circles">
+          <div className="window__circle window__circle_red"></div>
+          <div className="window__circle window__circle_yellow"></div>
+          <div className="window__circle window__circle_green"></div>
         </div>
-        <div className={`notes__full${props.menuOpened ? ' is-hidden' : ''}`}>
-          {props.activeNote ? <NoteItem note={props.activeNote} /> : ''}
+        <WindowMenu />
+        <p className="window__title" onClick={props.showMenu}>Заметки</p>
+      </div>
+      <div className="window__body">
+        <div className="notes">
+          <div className="notes__list">
+            {props.notes.map(note =>
+              <NoteMenuItem
+                key={note._id}
+                note={note}
+                onClick={props.onNoteClick}
+                activeSlug={props.activeNote ? props.activeNote.slug : null }
+              />
+            )}
+          </div>
+          <div className={`notes__full${props.menuOpened ? ' is-hidden' : ''}`}>
+            {props.activeNote ? <NoteItem note={props.activeNote} /> : ''}
+          </div>
         </div>
       </div>
     </div>
-  </div>;
+  </Draggable>;
 
 export default NoteList;
